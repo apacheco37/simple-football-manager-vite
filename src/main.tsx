50544ce8dbd101fbe5d3b3fa0f879d53f46c8f79
Andres Pacheco <apacheco73@hotmail.com>
2023-07-24 23:25:00 -0300

    Dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,21 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
