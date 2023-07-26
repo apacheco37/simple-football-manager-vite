ae1654b09d6cbf956ba43d67d46efa72c92f49ae
Andres Pacheco <apacheco73@hotmail.com>
2023-07-25 22:29:37 -0300

    Header

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,11 +1,24 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <Box sx={{ padding: "2rem" }}>
-      <Outlet />
-    </Box>
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Simple Football Manager
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Toolbar />
+        {/* https://mui.com/material-ui/react-app-bar/#fixed-placement */}
+      </Box>
+      <Box sx={{ padding: "2rem" }}>
+        <Outlet />
+      </Box>
+    </>
   );
 };
 
