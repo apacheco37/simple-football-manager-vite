278e8bb594e74443dc36f5be6a781bdb27fdb926
Andres Pacheco <andres.pacheco@orangeloops.com>
2025-01-26 11:33:14 -0300

    Sidebar first version

--- src/pages/savegame-layout.tsx
+++ src/pages/savegame-layout.tsx
@@ -1,22 +1,63 @@
 import { useLiveQuery } from "dexie-react-hooks";
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useNavigate, useParams } from "react-router-dom";
 
 import { SaveGame, db } from "../db/db";
 import { createContext } from "react";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
 
 export const SaveGameContext = createContext<{ saveGame: SaveGame }>(
   {} as { saveGame: SaveGame }
 );
 
+const drawerWidth = 180;
+
 const SaveGameLayout = () => {
   const { savegameid } = useParams();
   const saveGame = useLiveQuery(() => db.saveGame.get(Number(savegameid)));
+  const navigate = useNavigate();
+  // const [isSidebarOpen, setIsSidebarOpen] = useState(true);
 
   if (!saveGame) return <></>;
 
   return (
     <SaveGameContext.Provider value={{ saveGame }}>
-      <Outlet />
+      <Drawer
+        variant="persistent"
+        open={true}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        <Toolbar />
+        <List>
+          <ListItem key={"Teams"} disablePadding>
+            <ListItemButton onClick={() => navigate("teams")}>
+              <ListItemText primary={"Teams"} />
+            </ListItemButton>
+          </ListItem>
+          <ListItem key={"Players"} disablePadding>
+            <ListItemButton /* onClick={() => navigate("players")} */>
+              <ListItemText primary={"Players"} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+      <Box sx={{ marginLeft: `${drawerWidth}px` }}>
+        <Outlet />
+      </Box>
     </SaveGameContext.Provider>
   );
 };
